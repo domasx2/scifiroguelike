426e3886386c02afcd6c707d571c763545fd08b2
Domas <domasx2@gmail.com>
2013-01-30 23:31:15 +0200

    oh sheet a syntax error :D

--- javascript/settings.js
+++ javascript/settings.js
@@ -3,4 +3,4 @@ exports.ZOOM = 2;
 exports.TILE_WIDTH = 16;
 exports.MOVE_DURATION = 400; //ms; how long it takes for a creature to move one tile
 exports.FPS = 30;
-exports.VERSION = 0.06.1;
+exports.VERSION = "0.06.1";
